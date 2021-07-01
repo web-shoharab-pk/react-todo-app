49627c9845dfe98caa2b4f19951892b3d26fb0f8
md shoharab pk <shoharabpk.2019@gmail.com>
2021-07-01 11:50:29 +0600

    done

---
+++ build/precache-manifest.179df4a05ce9e5ef74fe3a1f6f344d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724055d5292665da0b12b362ef732be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa25e0c8ad2c341abbe",
+    "url": "/static/css/main.aa1c3f4a.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa25e0c8ad2c341abbe",
+    "url": "/static/js/main.888a27f4.chunk.js"
+  },
+  {
+    "revision": "ed72fd0300eba1b1f74d",
+    "url": "/static/js/runtime-main.258720a7.js"
+  }
+]);
